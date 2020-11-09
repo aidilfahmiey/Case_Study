3575ba90415c32674553b184dc7182ab134e6b56
aidilfahmiey <aidil.fahmiey@live.iium.edu.my>
2020-11-09 16:34:15 +0800

    few changes

---
+++ Reservation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Gaffer</title>
+    <link rel="stylesheet" href="styling/Reservation.css">
+</head>
+<body>
+    <header>
+        <div class="Top">
+        <img src="resource/logo1.png" alt="logo" class="logo">
+            <nav>
+                <ul>
+                <li><a href="index.html"><strong>Home</strong></a></li>
+                <li><a href="Reservation.php"><strong>Reservation</strong></a></li>
+                <li><a href="About.php"><strong>About Us</strong></a></li>
+                <li><a href="Contact.php"><strong>Contact Us</strong></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+
+</body>
+</html>
