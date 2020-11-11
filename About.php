e58769390753c728d0881a2271520919587b5267
sabrinafasha25 <sabrina.fasha@live.iium.edu.my>
2020-11-11 16:15:58 +0800

    Updated Version of About Us Page

---
+++ About.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Gaffer</title>
+    <link rel="stylesheet" href="styling/About.css">
+</head>
+<body>
+    <header>
+        <div class="Top">
+        <a href="index.html" class="logomain"><img src="resource/logo1.png" alt="logo" class="logo"></a>
+        
+            <nav>
+                <ul>
+                <li><a href="index.html"><strong>Home</strong></a></li>
+                <li><a href="Reservation.php"><strong>Reservation</strong></a></li>
+                <li><a href="About.php"><strong>About Us</strong></a></li>
+                <li><a href="Contact.php"><strong>Contact Us</strong></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <hr class="rounded">
+
+    <img src="About.jpg" alt="About" width="1250" height="350"><br>
+    
+<body>
+    <div class="About">
+       EventGaffer is a global self-service ticketing solution forevents ranging from small congregational to big conferences as well as music concerts, marathons, fundraising program and gaming competitions. Now everyone can organize, discover, book and join events that encourage their joy and fulfill their lives. Our purpose is to connect the world together through live escapade.
+    </div>
+
+    <br>
+
+    <h2>OUR HISTORY</h2>
+    <div class="History">
+        In today’s world of technology, everyone has becoming familiar with electronic commerce that enable users to book discovered event tickets as well as revolutionize event organizers’ marketing approach. However, to be exact, distributed commerce has been evolving for awhile which many technological expansions have made it all achievable. Inspired by Ticketmaster in the late 80s where people used to line up at the box office to buy tickets which then, the industry has moved to a physical distributed commerce platform for instance at record stores or venues. After that, ticketing shifted online through specific commerce platform and now we are in the era of new model of distributed commerce conducted online which in other words physical “expansion” back then in the late 80s but only that everything is online which in other word we are distributing event tickets across digital platforms. In fact, with the mobile advancement we have, users are allowed to keep their virtual tickets on their phones while the organizers can sell tickets from their phones. At EventGaffer, we believe that staying ahead of mobile advancements means revolving the way products are built. Our vision is to provide a platform where anyone can easily build on, so that we can give our users the best experience possible. Essentially, we embrace partnerships that give our organizers the best tools out there for growing their businesses and that make it as easy as possible for attendees to buy tickets. When it comes to distributed commerce, this means helping organizers gain incremental ticket sales by keeping their customer right where they are from discovery through purchase. Ten years from now, we hope that EventGaffer will be recognized as a trusted source which user found it both a safe and easy transaction system.
+    </div>
+    <div class="Balloon">
+        <img src="History.jpg" ALIGN="right" width="300" height="300">  
+    </div>
+
+    <h2>OUR TEAM</h2>
+    <p></p>
+</body>
+
+</body>
+</html>
---
+++ Contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Gaffer</title>
+    <link rel="stylesheet" href="styling/Reservation.css">
+</head>
+<body>
+    <header>
+        <div class="Top">
+        <a href="index.html" class="logomain"><img src="resource/logo1.png" alt="logo" class="logo"></a>
+        
+            <nav>
+                <ul>
+                <li><a href="index.html"><strong>Home</strong></a></li>
+                <li><a href="Reservation.php"><strong>Reservation</strong></a></li>
+                <li><a href="About.php"><strong>About Us</strong></a></li>
+                <li><a href="Contact.php"><strong>Contact Us</strong></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <hr class="rounded">
+
+</body>
+</html>
