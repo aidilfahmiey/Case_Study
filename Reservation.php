68e3dafeb2af8df1d6a86b898ed42447cad7bac7
aidilfahmiey <aidil.fahmiey@live.iium.edu.my>
2020-11-12 16:21:20 +0800

    updated by aidil

--- Reservation.php
+++ Reservation.php
@@ -58,6 +58,32 @@
             </form>
         </div>
     </div>
+   
+    <table class="table">
+    <tr>
+        <td width=50%>
 
+        <div class="text">
+            <a>Jalan Hang Tuah,</a>
+            <br>
+            <a>Persiaran Tunku Mahkota Patik,</a>
+            <br>
+            <a>40460 Kota Kemuning, Shah Alam</a>
+            <br>
+            <a>Selangor, Malaysia</a>
+        </div>
+
+        </td>
+        <td>
+
+        <div class="follow">
+            <a class="footpic">Follow us</a>
+            <br><br>
+            <a href="https://twitter.com/explore"><img src="resource/twitter.png" alt="Twitter logo" class="footpic"></a>
+            <a  href="https://www.facebook.com/"><img src="resource/fb.png" alt="Facebook logo" class="footpic"></a>
+        </div>
+
+        </td>
+    </tr>
 </body>
 </html>
