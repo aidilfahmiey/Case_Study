babd38c7325fc17686cd51d6c9f7cf681c0c60a7
aidilfahmiey <aidil.fahmiey@live.iium.edu.my>
2020-11-16 15:42:27 +0800

    update by aidil

--- script/storedata.php
+++ script/storedata.php
@@ -1,4 +1,19 @@
 <?php
 
 
+if(isset($_POST['submit']) && $_POST['submit']){
+
+        $fname= $_POST['first_name'];
+        $lname= $_POST['last_name'];
+        $email= $_POST['email'];
+        $phonenumber= $_POST['Phone_number'];
+        $datec= $_POST['datec'];
+        $ic= $_POST['ic'];
+        $stime= $_POST['starttime'];
+        $etime= $_POST['endtime'];
+        $pax= $_POST['pax'];
+
+}
+
+
 ?>
